4fc53b0acd70e8969020a6e37af56d0cfafd0bde
techLeopard <adilet.satylganov@gmail.com>
2025-07-30 17:51:30 +0600

    vercel deploy fix

--- src/main/java/com/techleopard/plutusstash/model/User.java
+++ src/main/java/com/techleopard/plutusstash/model/User.java
@@ -1,13 +1,11 @@
 package com.techleopard.plutusstash.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "users")
 public class User {
 
     @Id
